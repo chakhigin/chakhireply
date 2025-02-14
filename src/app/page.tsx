223c52fa5435ef48f217578592852b26f4033c04
chakhigin <ehsankhforex@gmail.com>
2025-02-15 00:08:35 +0330

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <button onClick={logIn}>
         Sign In
       </button>
-      <Link href="https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=28278206125157935&redirect_uri=https://localhost:3000/&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights">
+      <Link href="https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=28278206125157935&redirect_uri=https://chakhireplay.netlify.app/&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights">
         instagram
       </Link>
       <button onClick={sendDm}>
