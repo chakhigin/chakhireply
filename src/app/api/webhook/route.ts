b1d28492cdeb1e655fb5af83cb15446c2d03f483
chakhigin <ehsankhforex@gmail.com>
2025-02-15 14:06:51 +0330

    add webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,5 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export interface WebhookPayload {
     object: string;
@@ -31,30 +30,26 @@ export interface UserDetails {
     profile_pic?: string;
 }
 
-export default async function handler(req:NextRequest, res: NextApiResponse) {
-    if (req.method === 'GET') {
+export async function GET(req:NextRequest) {
         const mode = req.nextUrl.searchParams.get("hub.mode") as string;
         const token = req.nextUrl.searchParams.get("hub.verify_token") as string;
         const challenge = req.nextUrl.searchParams.get("hub.challenge") as string;
-        return res.status(200).send(challenge);
-    } else if (req.method === 'POST') {
-        const body = await req.json();
-        console.log('Webhook received:', JSON.stringify(body, null, 2));
+        return new NextResponse(challenge);
+}
 
-        if (body.object === 'instagram' && body.entry) {
-            for (const entry of body.entry) {
-                for (const messagingEvent of entry.messaging) {
-                    const senderId = messagingEvent.sender.id;
-                    const messageText = messagingEvent.message?.text;
+export async function POST(req:NextRequest){
+    const body = await req.json();
+    console.log('Webhook received:', JSON.stringify(body, null, 2));
 
-                    console.log(`Message from user ID: ${senderId}`);
-                    console.log(`Message text: ${messageText}`);
-                }
+    if (body.object === 'instagram' && body.entry) {
+        for (const entry of body.entry) {
+            for (const messagingEvent of entry.messaging) {
+                const senderId = messagingEvent.sender.id;
+                const messageText = messagingEvent.message?.text;
+
+                console.log(`Message from user ID: ${senderId}`);
+                console.log(`Message text: ${messageText}`);
             }
         }
-
-        return res.status(200).end();
-    } else {
-        return res.status(405).end();
     }
 }
